fdbb731a7290df58dbf05f29f7b37ba330c4d0b5
Rostislav-Myasnikov <operkaka4@gmail.com>
2026-03-02 20:29:09 +0300

    Fix destructuring of noteId from req.params

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -33,7 +33,7 @@ export const deleteNote = async (req, res) => {
 };
 
 export const updateNote = async (req, res) => {
-  const { noteId } = await req.params;
+  const { noteId } = req.params;
   const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, {
     new: true,
   });
