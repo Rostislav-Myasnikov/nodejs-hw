4af201238e548097c19ca689d165b82c13e13d29
Rostislav <operkaka4@gmail.com>
2026-03-20 21:01:03 +0300

    add upload img and email verification

---
+++ src/utils/sendMail.js
@@ -0,0 +1,14 @@
+import nodemailer from 'nodemailer';
+
+const tranporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export const sendEmail = async (options) => {
+  return await tranporter.sendMail(options);
+};
